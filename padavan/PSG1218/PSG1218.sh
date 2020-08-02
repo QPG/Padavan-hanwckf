4eda3d1208fc35c4e65f5da3dbc8c5c111dd5225
chenxudong2020 <872603935@qq.com>
2020-08-02 13:56:49 +0800

    Update PSG1218.sh

--- padavan/PSG1218/PSG1218.sh
+++ padavan/PSG1218/PSG1218.sh
@@ -1,2 +1,10 @@
 # 是否超频 
 # echo "CONFIG_FIRMWARE_CPU_600MHZ=y" >> .config
+default_path='./user/shared/defaults.h'  # 默认文件配置目录
+echo "执行如下命令sed -i \"s/BOARD_PID/\"PHICOMM\"/g\" $default_path"
+sed -i "s/BOARD_PID/\"PHICOMM\"/g" $default_path
+default_theme_path='./user/www/n56u_ribbon_fixed'
+sudo rm -rf $default_theme_path/bootstrap
+sudo rm -rf $default_theme_path/device-map
+sudo rm -rf $default_theme_path/images
+cp -rf ../theme/. $default_theme_path
