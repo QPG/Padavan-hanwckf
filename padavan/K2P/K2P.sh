d9288d7304074b67ebc22f38d87c89cbe0708444
chenzc@prolog-int.com <chenzc@prolog-int.com>
2020-08-20 23:01:34 +0800

    fix language support

--- padavan/K2P/K2P.sh
+++ padavan/K2P/K2P.sh
@@ -1,6 +1,7 @@
 default_path='./user/shared/defaults.h'  # 默认文件配置目录
 echo "执行如下命令sed -i \"s/BOARD_PID/\"PHICOMM\"/g\" $default_path"
 sed -i "s/BOARD_PID/\"PHICOMM\"/g" $default_path
+sed -i "s/US/\"CN\"/g" $default_path
 default_theme_path='./user/www/n56u_ribbon_fixed'
 sudo rm -rf $default_theme_path/bootstrap
 sudo rm -rf $default_theme_path/images
