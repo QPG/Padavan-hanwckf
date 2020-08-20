647124cecbe8aaa3333d8ac685363e8e688a5961
chenzc@prolog-int.com <chenzc@prolog-int.com>
2020-08-20 23:26:34 +0800

    Revert "fix"
    This reverts commit cbbfce52b9cab92fc452fa2e281c20d6a8fac44d.

--- padavan/public/defaults.h
+++ padavan/public/defaults.h
@@ -18,8 +18,6 @@
 #ifndef _defaults_h_
 #define _defaults_h_
 
-#include <ralink_board.h>
-
 #define SYS_SHELL		"/bin/sh"
 #define SYS_EXEC_PATH		"/usr/sbin:/usr/bin:/sbin:/bin"
 #define SYS_EXEC_PATH_OPT	"/opt/sbin:/opt/bin:/usr/sbin:/usr/bin:/sbin:/bin"
@@ -30,25 +28,29 @@
 #define SYS_USER_NOBODY		"nobody"
 #define SYS_GROUP_NOGROUP	"nogroup"
 
-#define DEF_LAN_ADDR		"192.168.2.1"
-#define DEF_LAN_DHCP_BEG	"192.168.2.100"
-#define DEF_LAN_DHCP_END	"192.168.2.244"
+#define DEF_LAN_ADDR		"192.168.1.1"
+#define DEF_LAN_DHCP_BEG	"192.168.1.2"
+#define DEF_LAN_DHCP_END	"192.168.1.244"
 #define DEF_LAN_MASK		"255.255.255.0"
 
-#define DEF_WLAN_2G_CC		"CN"
-#define DEF_WLAN_5G_CC		"CN"
-#define DEF_WLAN_2G_SSID	BOARD_PID "_%s"
-#define DEF_WLAN_5G_SSID	BOARD_PID "_5G_%s"
-#define DEF_WLAN_2G_GSSID	BOARD_PID "_GUEST_%s"
-#define DEF_WLAN_5G_GSSID	BOARD_PID "_GUEST_5G_%s"
+#define DEF_WLAN_2G_CC		"GB"
+#define DEF_WLAN_5G_CC		"GB"
+#define DEF_WLAN_2G_SSID	"ASUS"
+#define DEF_WLAN_5G_SSID	"ASUS_5G"
+#define DEF_WLAN_2G_GSSID	"ASUS_GUEST"
+#define DEF_WLAN_5G_GSSID	"ASUS_GUEST_5G"
 #define DEF_WLAN_2G_PSK		"1234567890"
 #define DEF_WLAN_5G_PSK		"1234567890"
 
 #define DEF_ROOT_PASSWORD	"admin"
 #define DEF_SMB_WORKGROUP	"WORKGROUP"
-#define DEF_TIMEZONE		"CST-8"
-#define DEF_NTP_SERVER0		"ntp1.aliyun.com"
-#define DEF_NTP_SERVER1		"2001:470:0:50::2"
+#define DEF_TIMEZONE		"GMT0"
+#define DEF_NTP_SERVER0		"pool.ntp.org"
+#define DEF_NTP_SERVER1		"time.nist.gov"
+#ifdef SUPPORT_OPENSSL_EC
+#define DEF_HTTPS_CIPH_LIST	"ECDH+CHACHA20:ECDH+AESGCM:DH+AESGCM:DH+AES256:DH+AES:DH+3DES:RSA+AES:RSA+3DES:!ADH:!MD5:!DSS"
+#else
 #define DEF_HTTPS_CIPH_LIST	"DH+AESGCM:DH+AES256:DH+AES:DH+3DES:RSA+AES:RSA+3DES:!ADH:!MD5:!DSS"
+#endif
 
 #endif
